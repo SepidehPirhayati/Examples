60fdfea76bed6a8b6fd127b97e36fc35ddba8ee7
Sepideh <Sepidehpirhayati@gmail.com>
2019-12-06 06:32:32 +0330

    print the area and perimeter of a circle

---
+++ src/Sample3.java
@@ -0,0 +1,19 @@
+//Write a Java program to print the area and perimeter of a circle
+
+
+import java.util.Scanner;
+
+public class Sample3 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("input radius : ");
+        double input = in.nextDouble();
+
+        System.out.println("Perimeter : " + 2 * Math.PI * input );
+        System.out.println("Area : " + Math.PI * input * input);
+    }
+
+
+}
