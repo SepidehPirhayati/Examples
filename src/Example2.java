72244bb290176d768af15e93010c0cf5896aaa46
Sepideh <Sepidehpirhayati@gmail.com>
2019-12-09 23:17:45 +0330

    prints multiplication table upto 10

---
+++ src/Example2.java
@@ -0,0 +1,17 @@
+//Write a Java program that takes a number as input and prints its multiplication table upto 10
+
+import java.util.Scanner;
+
+public class Example2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println(" input number : ");
+        int num = input.nextInt();
+
+        for (int i = 1 ; i <=10 ; i++ ){
+            System.out.println( num + " * " + i +" = " + num*i );
+        }
+    }
+}
+
