e3c5900da5eb0d66128f56e7f49d5042c19fabf2
Sepideh <Sepidehpirhayati@gmail.com>
2019-12-10 19:36:11 +0330

    convert seconds to h, min and sec.

---
+++ src/Example12.java
@@ -0,0 +1,20 @@
+//Write a Java program to convert seconds to hour, minute and seconds.
+import java.util.Scanner;
+
+public class Example12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("input time in seconds : ");
+        int time = in.nextInt();
+        int h, m, s;
+        //hour
+        h=time/3600;
+        time%=3600;
+        //minute
+        m=time/60;
+        time%=60;
+        //second
+        s=time;
+        System.out.println(h +" : "+ m + " : " + s );
+    }
+}
