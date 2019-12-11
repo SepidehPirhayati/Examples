4609a20c91d94d14a54d1835cec9e32c3069be75
Sepideh <Sepidehpirhayati@gmail.com>
2019-12-11 22:45:45 +0330

    create a new string of n copies of the last 3 characters of the original string

--- src/Example20.java
+++ src/Example20.java
@@ -1,3 +1,18 @@
-//create a new string of 4 copies of the last 3 characters of the original string
+/*Write a Java program to create a new string of n copies of the last 3 characters of the original string.
+The length of the original string must be 3 and above.*/
+import java.util.Scanner;
+
 public class Example20 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String str = in.nextLine();
+        System.out.println("Enter number of copies");
+        int num = in.nextInt();
+        String substring = str.substring(str.length() - 3);
+        StringBuilder temp= new StringBuilder();
+        for (int i = 0 ; i < num ; i++)
+            temp.append(substring);
+        System.out.println(temp);
+    }
 }
