37af823a71182e12859249ffdebeec36264cd429
Sepideh <Sepidehpirhayati@gmail.com>
2019-12-11 22:19:23 +0330

    print an American flag on the screen

--- src/Example04.java
+++ src/Example04.java
@@ -1,7 +1,6 @@
 //Write a Java program to print an American flag on the screen
 public class Example04 {
     public static void main(String[] args) {
-
         for (int i = 1 ; i<=9 ; i++ ){
             if(i%2==1)
                 for (int j = 1 ; j <=6 ; j++)
